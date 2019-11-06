9ec7cecf24c41fdbe627cc0fe82a84397777171c
Thales Henrique <thales.henrique@engenharia.ufjf.br>
2019-11-06 00:00:35 -0300

    main updated

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    NAOKin nao(string("127.0.0.1"));
+    NAOKin nao(string("127.0.0.1")); 
 
-    nao.WakeUp();
+    nao.WakeUp(); //Wake up robot
     nao.GoToPosture("StandInit");
     nao.DoTrajectory();
     //nao.timer.Wait(2000);
